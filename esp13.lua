9a21bf0bbd3367aa25aa09e15b5acef88b6c257d
TroXy666 <cimmiriggi@gmail.com>
2026-02-06 17:09:24 +0300

    Update and rename esp12.lua to esp13.lua

--- esp13.lua
+++ esp13.lua
@@ -19,10 +19,50 @@ local ESPTab = Window:CreateTab("ESP")
 
 ESPTab:CreateSection("Основные настройки ESP")
 
-ESPTab:CreateToggle({
-   Name = "turn ESP",
-   CurrentValue = false,
-   Flag = "ESP_Enabled"
+local Button = Tab:CreateButton({
+   Name = "Button Example",
+   Callback = function()
+         
+while wait(0.5) do
+    for i, box in ipairs(workspace:GetDescendants()) do
+        if box:FindFirstChild("Humanoid") then
+            if not box:FindFirstChild("EspBox") then
+                if box ~= game.Players.LocalPlayer.Character then
+                    local esp = Instance.new("BoxHandleAdornment",box)
+                    esp.Adornee = box
+                    esp.ZIndex = 0
+                    esp.Size = Vector3.new(5, 6, 2)
+                    esp.Transparency = 0.5 --Changes Transparency 
+                    esp.Color3 = Color3.fromRGB(0,255,0) --Color change
+                    esp.AlwaysOnTop = true
+                    esp.Name = "EspBox"
+                end
+            end
+        end
+    end
+end--[[
+	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
+]]
+while wait(0.5) do
+    for i, box in ipairs(workspace:GetDescendants()) do
+        if box:FindFirstChild("Humanoid") then
+            if not box:FindFirstChild("EspBox") then
+                if box ~= game.Players.LocalPlayer.Character then
+                    local esp = Instance.new("BoxHandleAdornment",box)
+                    esp.Adornee = box
+                    esp.ZIndex = 0
+                    esp.Size = Vector3.new(5, 6, 2)
+                    esp.Transparency = 0.5 --Changes Transparency 
+                    esp.Color3 = Color3.fromRGB(0,255,0) --Color change
+                    esp.AlwaysOnTop = true
+                    esp.Name = "EspBox"
+                end
+            end
+        end
+    end
+end
+            
+   end,
 })
 
 ESPTab:CreateToggle({
